9a846f06e028d94451f7233d8bb6d86e5aeef017
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2025-01-17 00:11:42 -0300

    chore: basic apiKey generation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const generatedApiKeys = [];
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import { randomBytes } from "crypto";
+
+const _generatedApiKeys = [];
+const _pixQrCodes = [];
+
+async function main() {
+  const apiKey = randomBytes(128).toString("base64");
+  console.log(`ApiKey: ${apiKey}`);
+}
+
+main();
