57fb0b381e4078ffaa858f9f52d702533b8b1350
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2025-01-17 01:04:29 -0300

    chore: basic express implementation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { randomBytes } from 'crypto';
+import express from 'express';
 
 const _generatedApiKeys = [];
 const _pixQrCodes = [];
@@ -7,6 +8,16 @@ async function main() {
   console.log('Running Woovi in memory api mock...');
   const apiKey = randomBytes(64).toString('base64');
   console.log(`ApiKey: ${apiKey}`);
+  const app = express();
+  const port = 3000;
+
+  app.get('/', async (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
 }
 
 main();
