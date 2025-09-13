8bd0e7ba14cc8ce9f277e8439806ab82cab799d2
Guilherme <guimedeiroxdev@gmail.com>
2025-09-13 15:28:38 -0300

    feat: add getCharge method to retrieve charge details by ID

--- src/methods/charge.methods.ts
+++ src/methods/charge.methods.ts
@@ -147,3 +147,41 @@ export const createChargeStatic = async (req: Request, res: Response) => {
         charge: data,
     });
 };
+
+const getChargeByID = (db: DatabaseProvider, correlationID: string): { data: Charge | null; error: string | null } => {
+
+    const result = db.getChargeByID(correlationID);
+
+    if(!result){
+        return {
+            data: null, 
+            error: 'Charge not found'
+        }
+    }
+
+    return{
+        data: result,
+        error: null
+    }
+
+}
+
+export const getCharge = async(req: Request, res: Response) => {
+
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).send({ error: 'Identifier is required' });
+      }
+
+    const db = req.context.db;
+
+    const result = getChargeByID(db, id);
+
+    if(!result.data){
+        return res.status(400).send({error: result.error});
+    }
+
+    return res.status(200).send(result.data);
+
+}
