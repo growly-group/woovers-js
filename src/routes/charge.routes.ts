b634e8ed946d37b21f86cdefe601f68c3eaa416a
Guilherme <guimedeiroxdev@gmail.com>
2025-09-13 15:29:10 -0300

    feat: add GET route for retrieving charge details by ID

--- src/routes/charge.routes.ts
+++ src/routes/charge.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createChargeStatic } from "../methods/charge.methods";
+import { createChargeStatic, getCharge } from "../methods/charge.methods";
 
 const router = Router(); 
 
 
 router.post('/charge', createChargeStatic);
+router.get('/charge/:id', getCharge);
+
 
 export default router;
