61d21d8535981fda7e0374f271b804ae85f04f50
guilherme <guimedeiroxdev@gmail.com>
2025-09-16 16:14:58 -0300

    feat: add GET route for retrieving all charges

--- src/routes/charge.routes.ts
+++ src/routes/charge.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createChargeStatic, getCharge } from "../methods/charge.methods";
+import { createChargeStatic, getCharge, getChargesStatic } from "../methods/charge.methods";
 
 const router = Router(); 
 
 
 router.post('/charge', createChargeStatic);
 router.get('/charge/:id', getCharge);
+router.get('/charge', getChargesStatic);
 
 
 export default router;
